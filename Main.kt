247f16e25ec8dc65f41762befc9f3dbd17870723
NLee <49750572+nl900@users.noreply.github.com>
2023-03-07 17:18:58 +0000

    Delete Main.kt

--- Main.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-  
-  // builder
-    val foodOrder = FoodOrder.Builder()
-        .bread("white")
-        .meat("bacon")
-        .condiments("olive oil")
-        .build()
-}
