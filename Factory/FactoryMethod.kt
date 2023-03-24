c9c788172520650cbe6877ab15ec698a2e00f1b8
NLee <49750572+nl900@users.noreply.github.com>
2023-03-24 13:44:09 +0000

    Update FactoryMethod.kt

--- Factory/FactoryMethod.kt
+++ Factory/FactoryMethod.kt
@@ -1,9 +1,15 @@
+/*
+When there's generic processing in a class but want to vary the kind class.
+It defines a method for creating objects which sublcasses override to change the type
+of objects to create
+ */
 
-
+// defines the type of objects to be created
 interface AlertDialog {
-    fun show()
+    fun show()  // defines the methods all objects created by the factory method must implement
 }
 
+// classes implement the interface define the type of objects created
 class MaterialAlertDialog : AlertDialog {
     override fun show() {
         println("Showing Material Design Alert Dialog")
@@ -16,9 +22,13 @@ class CupertinoAlertDialog : AlertDialog {
     }
 }
 
+// creator interface define the methods subclasses use to create objects
 interface DialogFactory {
     fun createAlertDialog(): AlertDialog
 }
+
+// implement the creator interface implement of the factory method and determine the
+// type of object created
 
 class MaterialDialogFactory : DialogFactory {
     override fun createAlertDialog(): AlertDialog {
