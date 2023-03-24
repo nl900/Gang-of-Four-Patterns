9b8bd1585cfaeca43eb45cde330f01422e59a6da
NLee <49750572+nl900@users.noreply.github.com>
2023-03-24 11:49:56 +0000

    Update Factory.kt

--- Factory.kt
+++ Factory.kt
@@ -9,3 +9,45 @@ class PartsFactory {
     fun makeEngine(): Engine { return Engine() }
     fun makeChassis(): Chassis { return Chassis() }
 }
+
+/*
+Abstract factory is used for dependency inection / strategy, to create a whole family of objects that need to be of the same kind
+and have common base classes
+An interface that allows a family of closely related classes to be instantiated
+The example below, the interface acts as a blueprint for what kind of car parts the independent factories should build and
+from what arguments. The factories / subclasses will then build the parts based on the requirements and processes specific
+ */
+abstract class CarPartsFactory {
+    abstract fun buildEngine(): Engine
+    abstract fun buildChassis(): Chassis
+
+}
+
+class CarPartsFactoryProvider {
+    // reified keyword allows the function to access the generic type M as a normal class in the function body at runTime
+    // otherwise it's only available at compile time and erased at runtime
+    inline fun <reified M : CarPartsFactory> createFactory(): CarPartsFactory {
+        return when (M::class) {
+            AudiPartsFactory::class -> AudiPartsFactory()
+            ToyotaPartsFactory::class -> ToyotaPartsFactory()
+            else -> throw IllegalArgumentException()
+        }
+    }
+}
+
+class AudiPartsFactory: CarPartsFactory() {
+    override fun buildEngine(): Engine { return Engine() }
+    override fun buildChassis(): Chassis { return Chassis() }
+}
+
+class ToyotaPartsFactory: CarPartsFactory() {
+    override fun buildEngine(): Engine { return Engine() }
+    override fun buildChassis(): Chassis { return Chassis() }
+}
+
+fun main(args: Array<String>) {
+    val factoryProvider = CarPartsFactoryProvider()
+    val partsFactory = factoryProvider.createFactory<AudiPartsFactory>()
+    val engine = partsFactory.buildEngine()
+    println("created engine: $engine")
+}
